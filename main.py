9f94439d12fabae1a7db9a33436f4e2ff4444440
Simon Moe Sørensen <moe.simon@gmail.com>
2018-02-20 08:29:36 +0100

    Added multiple classifiers

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pandas as pd
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 # Import data
 df = pd.read_csv("train.csv")
 
 # Assign features
-features = ["Sex", "Age", "SibSp", "Parch", "Pclass"]
+features = ["Embarked", "Pclass", "Sex", "Age", "Parch", "SibSp"]
 
 # Remove corrupt data
 df = df[features + ["Survived"]].dropna()
-
 
 # Convert Sex to binary, integers.
 # Male = 0, Female = 1
 df = df.replace(to_replace="male", value=0)
 df = df.replace(to_replace="female", value=1)
+
+# Convert embarked to C = 0, S = 1, Q = 2
+df = df.replace(to_replace="C", value=0)
+df = df.replace(to_replace="S", value=1)
+df = df.replace(to_replace="Q", value=2)
 
 # Split data into test and training data
 df_train = df.iloc[0:int(len(df) * 0.75), :]
@@ -28,11 +34,28 @@ X_train = df_train[features]
 y_train = df_train["Survived"]
 y_test = df_test["Survived"]
 
-# Create classififer, with default parameters
-
-svm = SVC()
+# Create svm classififer, with default parameters
+svm = SVC(C=1.5, kernel="rbf")
 
 # Fit data
 svm.fit(X_train, y_train)
 
 svm.score(X_test, y_test)
+
+# Create knn classififer
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Fit data
+knn.fit(X_train, y_train)
+
+# score
+knn.score(X_test, y_test)
+
+# Create dt classifier
+dt = DecisionTreeClassifier()
+
+# Fit data
+dt.fit(X_train, y_train)
+
+# score
+dt.score(X_test, y_test)
