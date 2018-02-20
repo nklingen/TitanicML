13068ec7b3c1105cc166e9b5768597bd2dda1534
Simon Moe Sørensen <moe.simon@gmail.com>
2018-02-20 18:04:57 +0100

    Began cross-validating the models and reflecting over them. Also sorted scores into proper tables

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ separates the data into training and testing data.
 """
 
 import pandas as pd
+from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -30,6 +31,16 @@ df = df.replace(to_replace="Q", value=2)
 # Set value to 0 if NaN, else 1
 df["Cabin"] = df["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
 
+# For Nat:
+# .apply method to the Series instance takes a function as the first argument
+# that's why we use the lambda function (a function without a name)
+# which basically iterates over the Series as in:
+# for elem in [1, 2, "str", 4]:
+#     if type(elem) == str:
+#         print("Omg mad stringz")
+#
+# Hope it makes sense, text me if it doesn't
+
 # Remove corrupt data
 df = df[features + ["Survived"]].dropna()
 
@@ -45,18 +56,50 @@ X_train = df_train[features]
 y_train = df_train["Survived"]
 y_test = df_test["Survived"]
 
+# Create data for Kfolds method
+X_kfold = df[features]
+y_kfold = df["Survived"]
+
 
 # Create classifier instances
-svm = SVC(C=1.5, kernel="rbf")
-knn = KNeighborsClassifier(n_neighbors=5)
-dt = DecisionTreeClassifier()
+svm_holdout = SVC(C=1.5, kernel="rbf")
+knn_holdout = KNeighborsClassifier(n_neighbors=5)
+dt_holdout = DecisionTreeClassifier()
+
+svm_kfold = SVC(C=1.5, kernel="linear")
+knn_kfold = KNeighborsClassifier(n_neighbors=5)
+dt_kfold = DecisionTreeClassifier()
 
 # Fit data to classifiers
-svm.fit(X_train, y_train)
-knn.fit(X_train, y_train)
-dt.fit(X_train, y_train)
+svm_holdout.fit(X_train, y_train)
+knn_holdout.fit(X_train, y_train)
+dt_holdout.fit(X_train, y_train)
 
-# Score models to test data
-svm.score(X_test, y_test)
-knn.score(X_test, y_test)
-dt.score(X_test, y_test)
+# Score models to test data, using the Holdout method
+svm_holdout_scores = svm_holdout.score(X_test, y_test)
+knn_holdout_scores = knn_holdout.score(X_test, y_test)
+dt_holdout_scores = dt_holdout.score(X_test, y_test)
+
+# Score models to using the KFold method
+svm_kfold_scores = cross_val_score(svm_kfold, X_kfold, y_kfold, cv=10)
+knn_kfold_scores = cross_val_score(knn_kfold, X_kfold, y_kfold, cv=10)
+dt_kfold_scores = cross_val_score(dt_kfold, X_kfold, y_kfold, cv=10)
+
+df_kfold_scores = pd.DataFrame(data={"SVM Scores": svm_kfold_scores,
+                                     "KNN Scores": knn_kfold_scores,
+                                     "DT Scores": dt_kfold_scores})
+
+df_holdout_scores = pd.DataFrame(data={"SVM Scores": svm_holdout_scores,
+                                       "KNN Scores": knn_holdout_scores,
+                                       "DT Scores": dt_holdout_scores},
+                                 index=["Accuracy"])
+
+print(df_holdout_scores)
+print("\n\n")
+print(df_kfold_scores)
+
+# Might be possible the model is overfitted a bit, we should look into
+# how to properly evaluate and fit the models with correct features
+# Also the Decision Tree needs less categories to decide from
+# Meaning all under age 16, gets value 0. Those between 16 and 25 gets
+# the value 1 and so on
